7fb152ead885e6ed9669e75aeaf1c227da84b9a0
Abdul Ahad56 <150322918+AbdulAhad4455@users.noreply.github.com>
2024-12-28 06:30:44 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let emailID =  "abdulahad@gmail.com"
+var accountPassword = "12345899"
+accountCity = "Karachi"
+
+//accountId = 2233
+emailID = "ansari@gmail.com"
+accountPassword="2244"
+accountCity = "Tando Adam"
+
+console.table([accountId,emailID,accountPassword,accountCity])
