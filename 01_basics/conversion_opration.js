d79e8365e791d5414b0914b795cdde11098a4f09
Abdul Ahad56 <150322918+AbdulAhad4455@users.noreply.github.com>
2024-12-29 05:18:14 +0000

    conversion DataTypes

---
+++ 01_basics/conversion_opration.js
@@ -0,0 +1,24 @@
+//let score = "33a" // lekin is ki value not a number ho gi NaN
+//let score = null
+//let score;
+//let score = true
+/*let score = "Abdul Ahad"
+console.log(typeof score)
+let num = Number(score)
+console.log(typeof num)
+console.log(num)*/
+
+
+
+//let isLogged;
+/*let isLogged = 8 // jab koe value hogi to ture ho sifay 0 ke or sifaye empty string ke or null ke
+//let isLogged = 0 // ""/ null
+let booleanVal = Boolean(isLogged)
+console.log(typeof booleanVal)
+console.log(booleanVal)*/
+
+let num = 33
+
+let strVal = String(num)
+console.log(typeof strVal)
+console.log(strVal)
