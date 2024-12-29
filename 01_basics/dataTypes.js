79666548685f7faaa5649636f84c43e0bb2e56da
Abdul Ahad56 <150322918+AbdulAhad4455@users.noreply.github.com>
2024-12-29 04:41:54 +0000

    DataTypes in Js

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,26 @@
+let age = 18 //Number
+let str = "AbdulAhad" // String
+let isLogged = true // Bolean
+let empt = null // null
+let st; // undefined
+
+//data Types
+//number => Rang is 2 power 53
+console.log(typeof age)
+console.log(age)
+//bigint => use for greaters number
+//String => ""
+console.log(typeof str)
+console.log(str)
+//Bolean => true / false
+console.log(typeof isLogged)
+console.log(isLogged)
+//Undefined => variable ki value declear nahi ki ho
+console.log(typeof st)
+console.log(st)
+//null => use for empty 
+console.log(typeof empt)//null ki type of object hoti hai
+console.log(empt)
+//symbol => use for uniquness
+
+// Object 
